6675f5394727e0f43b81e6c35eab8b4907c7b2e3
no7dw <no7david@gmail.com>
2019-05-20 15:34:36 +0800

    feat: demo

--- bin/1.sh
+++ bin/1.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 # db cashLoan 
 # collection users , loans
+#1
 #generate upsert.result
-mlogfilter mongod01.log --operation update >> updsert.result
-
+cd ../../mtools/
+mlogfilter mongod01.log --operation update > upsert.result
+#2
 #genreate varity.result
-mongo cashLoan --eval "var collection = 'users', lastValue = true, outputFormat='json'" variety.js >> varity.users.result.json
-mongo cashLoan --eval "var collection = 'loans', lastValue = true, outputFormat='json'" variety.js  >> varity.loans.result.json
+cd ../variety
+mongo cashLoan --eval "var collection = 'users', lastValue = true, outputFormat='json'" variety.js > variety0.users.result.json
+#3 remove head 22 line
+tail -n +23 variety0.users.result.json > variety.users.result.json 
 
+#repeat
+mongo cashLoan --eval "var collection = 'loans', lastValue = true, outputFormat='json'" variety.js  > variety0.loans.result.json
+tail -n +23 variety0.loans.result.json > variety.loans.result.json 
+
+mv variety.loans.result.json ../mongo-design/sample-data/
+cd ./mongo-design
+python src/array.py 
---
+++ bin/demo.md
@@ -0,0 +1,8 @@
+#1 analyse mongodb.log using mlogfilter(from mtools)
+
+#2 analyse documents data in collection using variety
+
+#3 run analyst script for some design rule 
+  
+
+
